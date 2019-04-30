ec150f2937c1b30c62a776747c16bc4e17e4322b
simonmonk <evilgeniusauthor@gmail.com>
2019-04-30 11:49:51 +0100

    fixed project 7 bug

--- micropython/p7_magic_music.py
+++ micropython/p7_magic_music.py
@@ -8,5 +8,6 @@ baseline_lightness = pin2.read_analog()
 while True:
     lightness = pin2.read_analog()
     tone = baseline_lightness - lightness
-    music.pitch(tone * multiplier)
+    if tone > 0:
+        music.pitch(tone * multiplier)
     sleep(100)
