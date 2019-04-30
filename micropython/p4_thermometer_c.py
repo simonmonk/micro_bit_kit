b93665eac2142250b479f45358c34f3b29390fc4
simonmonk <evilgeniusauthor@gmail.com>
2019-04-30 12:35:16 +0100

    put round fn to improve project 4

--- micropython/p4_thermometer_c.py
+++ micropython/p4_thermometer_c.py
@@ -2,6 +2,6 @@ from microbit import *
 
 while True:
     reading = pin1.read_analog()
-    temp_c = reading * 0.157 - 54
+    temp_c = round(reading * 0.157 - 54)
     display.scroll(str(temp_c))
     
--- micropython/p4_thermometer_f.py
+++ micropython/p4_thermometer_f.py
@@ -2,5 +2,5 @@ from microbit import *
 
 while True:
     reading = pin1.read_analog()
-    temp_c = round(reading * 0.135 +1)
-    display.scroll(str(temp_c))
+    temp_f = round(reading * 0.135 +1)
+    display.scroll(str(temp_f))
